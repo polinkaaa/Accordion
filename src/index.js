f4768fac9fc43da6043c4a41cb0f61d1ba346e75
polinkaaa <polina_kond@mail.ru>
2023-12-21 16:17:02 +0300

    add animation

--- src/index.js
+++ src/index.js
@@ -8,14 +8,15 @@ btn = document.querySelector('.main__btn_continue');
 
 tab.forEach(function (item, index) {
     item.addEventListener("click", function () {
-        accordion[index].classList.toggle('hidden');
-        item.classList.add('checked')
-        if (accordion[index].classList.contains('hidden')) {
+        toggleTwoClasses(accordion[index], "is-visible", "is-hidden", 500);
+        item.classList.add('checked');
+        if (accordion[index].classList.contains('is-hidden')) {
             arrow[index].style.transform = "rotateX(0deg)";
             item.style.border = "none";
             item.style.paddingLeft = "18px";
         } else {
             arrow[index].style.transform = "rotateX(180deg)";
+            item.style.transition = "0.6s";
             item.style.borderLeft = "4px solid #E9262D";
             item.style.paddingLeft = "14px";
         }
@@ -23,7 +24,19 @@ tab.forEach(function (item, index) {
     })
 })
 
+function toggleTwoClasses(element, first, second, timeOfAnimation) {
+    if (!element.classList.contains(first)) {
+      element.classList.add(first);
+      element.classList.remove(second);
+    } else {
+      element.classList.add(second);
+      window.setTimeout(function() {
+        element.classList.remove(first);
+      }, timeOfAnimation);
+    }
+  }
+
 function checkClick() {
-    const checked = document.querySelectorAll('.tab.checked ').length;
+    const checked = document.querySelectorAll('.tab.checked').length;
     tab.length === checked ? btn.disabled = false : '';
 }
